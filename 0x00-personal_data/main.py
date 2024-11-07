e05b057cf8060c7f4e66ad5e2ded9bebd89f78d0
wanjocharles <wanjocharles98@gmail.com>
2024-11-07 19:37:07 +0000

    main.py

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -3,12 +3,11 @@
 Main file
 """
 
-get_db = __import__('filtered_logger').get_db
+import logging
 
-db = get_db()
-cursor = db.cursor()
-cursor.execute("SELECT COUNT(*) FROM users;")
-for row in cursor:
-    print(row[0])
-cursor.close()
-db.close()
+get_logger = __import__('filtered_logger').get_logger
+PII_FIELDS = __import__('filtered_logger').PII_FIELDS
+
+print(get_logger.__annotations__.get('return'))
+print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+
