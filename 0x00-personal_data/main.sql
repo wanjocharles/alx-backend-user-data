856866839be2563ce14348bd52096f235f230d9f
wanjocharles <wanjocharles98@gmail.com>
2024-11-07 19:19:44 +0000

    main.py

--- 0x00-personal_data/main.sql
+++ 0x00-personal_data/main.sql
@@ -10,6 +10,3 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     email VARCHAR(256)
 );
-
-INSERT INTO users(email) VALUES ("bob@dylan.com");
-INSERT INTO users(email) VALUES ("bib@dylan.com");
