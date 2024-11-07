a9d0f7f9e915b97536766261673baa8def6bb4ba
wanjocharles <wanjocharles98@gmail.com>
2024-11-07 19:39:01 +0000

    README.md

--- 0x00-personal_data/main.sql
+++ 0x00-personal_data/main.sql
@@ -10,3 +10,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     email VARCHAR(256)
 );
+
+INSERT INTO users(email) VALUES ("bob@dylan.com");
+INSERT INTO users(email) VALUES ("bib@dylan.com");
